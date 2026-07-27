80aaa789553aac461603791d44d130615cd74de8
Efim <es.perevalov@mail.ru>
2026-07-27 15:14:33 +0300

    video/ssd0323: Implement the initialization function
    Signed-off-by: Efim <es.perevalov@mail.ru>

--- src/drivers/video/ssd0323/ssd0323.c
+++ src/drivers/video/ssd0323/ssd0323.c
@@ -16,6 +16,8 @@
 #include <hal/reg.h>
 #include <drivers/common/memory.h>
 #include <drivers/video/fb.h>
+#include <drivers/gpio.h>
+#include <drivers/spi.h>
 
 #include <framework/mod/options.h>
 #include <embox/unit.h>
@@ -24,6 +26,14 @@ EMBOX_UNIT_INIT(ssd0323_init);
 
 #define SSD0323_DISPLAY_WIDTH  OPTION_GET(NUMBER,display_width)
 #define SSD0323_DISPLAY_HEIGHT OPTION_GET(NUMBER,display_height)
+
+#define SSD0323_CS_PORT OPTION_GET(NUMBER,cs_port)
+#define SSD0323_CS_PIN  OPTION_GET(NUMBER,cs_pin)
+#define SSD0323_DC_PORT OPTION_GET(NUMBER,dc_port)
+#define SSD0323_DC_PIN  OPTION_GET(NUMBER,dc_pin)
+#define SSD0323_SPI_BUS	OPTION_GET(NUMBER,spi_bus)
+
+static struct spi_device *ssd0323_spi_dev;
 
 static int ssd0323_set_var(struct fb_info *info,
 		struct fb_var_screeninfo const *var) {
@@ -49,6 +59,13 @@ static const struct fb_ops ssd0323_ops = {
 };
 
 static int ssd0323_init(void) {
+	gpio_setup_mode(SSD0323_CS_PORT, 1 << SSD0323_CS_PIN, GPIO_MODE_OUT);
+	gpio_setup_mode(SSD0323_DC_PORT, 1 << SSD0323_DC_PIN, GPIO_MODE_OUT);
+
+	ssd0323_spi_dev = spi_dev_by_id(SSD0323_SPI_BUS);
+	if (!ssd0323_spi_dev){
+		return -ENODEV;
+	}
 
 	char *mmap_base = (void *) ssd0323_fb;
 
